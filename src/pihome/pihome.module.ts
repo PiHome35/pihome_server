51850fb10ad42f21521415c5f431ba02587cbafa
mahasachan <mahasachan.sa@ku.th>
2025-01-17 11:42:46 +0700

    Enhance PihomeModule with GraphQL support and integrate Langchain dependencies. Added ChatService and ChatResolver for chat functionalities. Updated package.json and pnpm-lock.yaml to include @langchain/community and @langchain/core, improving the project's capabilities for AI-driven features.

--- src/pihome/pihome.module.ts
+++ src/pihome/pihome.module.ts
@@ -6,12 +6,38 @@ import { UsersService } from './services/users.service';
 import { FamiliesController } from './controllers/families.controller';
 import { SpotifyConnectionsService } from './services/spotify-connections.service';
 import { DatabaseModule } from 'src/database/database.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ChatService } from './services/chat.service';
+import { ChatResolver } from './resolver/chat.resolver';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { UsersController } from './controllers/users.controller';
 
+const graphqlModule = GraphQLModule.forRoot<ApolloDriverConfig>({
+  driver: ApolloDriver,
+  autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+  installSubscriptionHandlers: true,
+  sortSchema: true,
+  playground: true,
+  subscriptions: {
+    'graphql-ws': true,
+    'subscriptions-transport-ws': true,
+  },
+  context: ({ req }) => ({ req }),
+});
+
 @Module({
-  imports: [DatabaseModule],
-  providers: [UsersService, FamiliesService, DeviceGroupsService, DevicesService, SpotifyConnectionsService],
   controllers: [FamiliesController, UsersController],
-  exports: [UsersService, FamiliesService, DeviceGroupsService, DevicesService, SpotifyConnectionsService],
+  imports: [DatabaseModule, graphqlModule],
+  providers: [
+    UsersService,
+    FamiliesService,
+    DeviceGroupsService,
+    DevicesService,
+    SpotifyConnectionsService,
+    ChatService,
+    ChatResolver,
+  ],
+  exports: [UsersService, FamiliesService, DeviceGroupsService, DevicesService, SpotifyConnectionsService, ChatService],
 })
 export class PihomeModule {}
